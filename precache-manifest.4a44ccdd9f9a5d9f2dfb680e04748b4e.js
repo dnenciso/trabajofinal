2fdfbe829419135d661cb5ee7157125aa2a73113
Daniel Nicolas Enciso <denciso@URBANIZADORA.local>
2021-05-01 15:48:06 -0500

    Updates

---
+++ precache-manifest.4a44ccdd9f9a5d9f2dfb680e04748b4e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8522c813a722fded5c1565a71dac2df",
+    "url": "/trabajofinal/index.html"
+  },
+  {
+    "revision": "f5fe16c64e617bf1b0a2",
+    "url": "/trabajofinal/static/css/main.69d06326.chunk.css"
+  },
+  {
+    "revision": "f235bd0a839c8b72f254",
+    "url": "/trabajofinal/static/js/2.2cc7ef6e.chunk.js"
+  },
+  {
+    "revision": "de1cedeacf4641d6277b42334cfacb1d",
+    "url": "/trabajofinal/static/js/2.2cc7ef6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5fe16c64e617bf1b0a2",
+    "url": "/trabajofinal/static/js/main.136c0232.chunk.js"
+  },
+  {
+    "revision": "7a48c8921b90cba9ed70",
+    "url": "/trabajofinal/static/js/runtime-main.16738d6e.js"
+  },
+  {
+    "revision": "ac7d30f522cc223eea0b05e982e0c5bc",
+    "url": "/trabajofinal/static/media/fondo.ac7d30f5.jpg"
+  }
+]);
